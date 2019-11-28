a844ce9caefacdb1ce8f4b0776d6e61eabc09395
Vinton Lee <vinton.e.lee@gmail.com>
2019-11-28 12:59:40 -0700

    database created

--- DB/eventdb.sql
+++ DB/eventdb.sql
@@ -7,6 +7,32 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema eventdb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `eventdb` ;
+
+-- -----------------------------------------------------
+-- Schema eventdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `eventdb` DEFAULT CHARACTER SET utf8 ;
+USE `eventdb` ;
+
+-- -----------------------------------------------------
+-- Table `Beverage`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Beverage` ;
+
+CREATE TABLE IF NOT EXISTS `Beverage` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `description` VARCHAR(400) NULL DEFAULT NULL,
+  `ingredients` TEXT(500) NULL DEFAULT NULL,
+  `caffeinated` TINYINT NOT NULL DEFAULT 0,
+  `contains_alcohol` TINYINT NOT NULL DEFAULT 0,
+  `calories` INT NOT NULL DEFAULT 0,
+  `volume` DOUBLE NOT NULL DEFAULT 8,
+  `active` TINYINT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
 SET SQL_MODE = '';
 DROP USER IF EXISTS eventuser@localhost;
 SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -17,3 +43,14 @@ GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'eventuser'@'localho
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `Beverage`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eventdb`;
+INSERT INTO `Beverage` (`id`, `name`, `description`, `ingredients`, `caffeinated`, `contains_alcohol`, `calories`, `volume`, `active`) VALUES (1, 'water', 'H2O from the tap', 'water', DEFAULT, DEFAULT, 0, 8, DEFAULT);
+INSERT INTO `Beverage` (`id`, `name`, `description`, `ingredients`, `caffeinated`, `contains_alcohol`, `calories`, `volume`, `active`) VALUES (2, 'coffee', 'brewed fresh', 'water and coffee', 1, DEFAULT, 0, 8, DEFAULT);
+
+COMMIT;
+
