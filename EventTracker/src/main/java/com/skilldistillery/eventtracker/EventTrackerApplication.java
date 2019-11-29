2f4e034899d0fed6af285f73a3f53324d7ff0a8b
Vinton Lee <vinton.e.lee@gmail.com>
2019-11-28 20:46:08 -0700

    added Deploying Spring Boot Apps items

--- EventTracker/src/main/java/com/skilldistillery/eventtracker/EventTrackerApplication.java
+++ EventTracker/src/main/java/com/skilldistillery/eventtracker/EventTrackerApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.eventtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EventTrackerApplication {
+public class EventTrackerApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EventTrackerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventTrackerApplication.class, args);
