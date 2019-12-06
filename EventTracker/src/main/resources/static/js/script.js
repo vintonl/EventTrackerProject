084dfa14091c4ada182c68095be600b80f34e75e
Vinton Lee <vinton.e.lee@gmail.com>
2019-12-06 16:48:03 -0700

    intial for js and index page

---
+++ EventTracker/src/main/resources/static/js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function(e) {
+	console.log('document loaded');
+	init();
+});
+
+function init() {
+//	document.filmForm.lookup.addEventListener('click', function(event) {
+//		event.preventDefault();
+//		var filmId = document.filmForm.filmId.value;
+//		if (!isNaN(filmId) && filmId > 0) {
+//			getFilm(filmId);
+//		}
+//	})
+//
+//	document.filmPost.create.addEventListener('click', function(event) {
+//		event.preventDefault();
+//		addNewFilm();
+//
+//	})
+}
