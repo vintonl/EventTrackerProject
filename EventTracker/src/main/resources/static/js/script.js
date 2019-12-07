559c8fab36fedd2736d4e17e0fa65a8253717797
Vinton Lee <vinton.e.lee@gmail.com>
2019-12-07 12:55:49 -0700

    table created with listener to display details

--- EventTracker/src/main/resources/static/js/script.js
+++ EventTracker/src/main/resources/static/js/script.js
@@ -44,6 +44,7 @@ function getAllBevs() {
 			let h1 = document.createElement('h1');
 			dataDiv.appendChild(h1);
 			h1.textContent = "Beverages not found.";
+
 		}
 	};
 
@@ -54,47 +55,42 @@ function displayBeverages(bevs) {
 	var dataDiv = document.getElementById('bevData');
 	dataDiv.textContent = '';
 
-	for (var i = 0; i < bevs.length; i++) {
-		let h1 = document.createElement('h1');
-		dataDiv.appendChild(h1);
-		h1.textContent = bevs[i].name;
+	let br = document.createElement('br');
+	dataDiv.appendChild(br);
+	let hr = document.createElement('hr');
+	dataDiv.appendChild(hr);
+	let table = document.createElement('table');
+	dataDiv.appendChild(table);
 
-		let descriptionP = document.createElement('p');
-		dataDiv.appendChild(descriptionP);
-		descriptionP.textContent = bevs[i].description;
+	bevs.forEach(function(bev, index, arr) {
+		let tr = document.createElement('tr');
+		let td = document.createElement('td');
+		td.textContent = bev.name;
 
-		let ul = document.createElement('ul');
-		let li = document.createElement('li');
-		li.textContent = "Ingredients: " + bevs[i].ingredients;
-		let li1 = document.createElement('li');
-		li1.textContent = "Caffeine: " + bevs[i].caffeine + " mg";
-		let li2 = document.createElement('li');
-		li2.textContent = "Calories: " + bevs[i].calories;
-		let li3 = document.createElement('li');
-		li3.textContent = "Volume: " + bevs[i].volume + " ounces";
-		let li4 = document.createElement('li');
-		li4.textContent = "Date: " + bevs[i].createdAt;
+		td.addEventListener('click', function(e) {
+			displayBev(bev);
+		});
+		dataDiv.appendChild(td);
+		dataDiv.appendChild(tr);
+	})
 
-		dataDiv.appendChild(li);
-		dataDiv.appendChild(li1);
-		dataDiv.appendChild(li2);
-		dataDiv.appendChild(li3);
-		dataDiv.appendChild(li4);
-		dataDiv.appendChild(ul);
-	}
+	let hr1 = document.createElement('hr');
+	dataDiv.appendChild(hr1);
 }
 
 function addNewBeverage() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', 'http://localhost:8083/api/beverages', true);
 
-	xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON request body
+	xhr.setRequestHeader("Content-type", "application/json"); // Specify JSON
+	// request body
 
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
 			if (xhr.status == 200 || xhr.status == 201) { // Ok or Created
 				var data = JSON.parse(xhr.responseText);
 				console.log(data);
+				getAllBevs();
 			} else {
 				console.log("POST request failed.");
 				console.error(xhr.status + ': ' + xhr.responseText);
@@ -118,7 +114,13 @@ function addNewBeverage() {
 		}
 	};
 
-	var userObjectJson = JSON.stringify(newBevObject); // Convert JS object to JSON string
+	var userObjectJson = JSON.stringify(newBevObject); // Convert JS object to
+	// JSON string
 
 	xhr.send(userObjectJson);
+
+}
+
+function displayBev(bev) {
+	console.log("displayBev " + bev.name);
 }
