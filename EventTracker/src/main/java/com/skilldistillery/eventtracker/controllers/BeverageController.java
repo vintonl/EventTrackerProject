7bc4a683815457155525422eec748b27858c771a
Vinton Lee <vinton.e.lee@gmail.com>
2019-12-13 15:42:42 -0700

    initial for angular front end

--- EventTracker/src/main/java/com/skilldistillery/eventtracker/controllers/BeverageController.java
+++ EventTracker/src/main/java/com/skilldistillery/eventtracker/controllers/BeverageController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.skilldistillery.eventtracker.services.BeverageService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin({ "*", "http://localhost:4209" })
 public class BeverageController {
 
 	@Autowired
