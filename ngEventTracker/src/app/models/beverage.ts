7e1a91fc60fdb51ee0e686589a687b8dd07e5d6a
Vinton Lee <vinton.e.lee@gmail.com>
2019-12-13 17:03:41 -0700

    completed beverage model

--- ngEventTracker/src/app/models/beverage.ts
+++ ngEventTracker/src/app/models/beverage.ts
@@ -1,2 +1,42 @@
 export class Beverage {
+  id: number;
+  name: string;
+  description: string;
+  ingredients: string;
+  caffeinated: boolean;
+  caffeine: number;
+  containsAlcohol: boolean;
+  calories: number;
+  volume: number;
+  active: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(
+    id?: number,
+    name?: string,
+    description?: string,
+    ingredients?: string,
+    caffeinated?: boolean,
+    caffeine?: number,
+    containsAlcohol?: boolean,
+    calories?: number,
+    volume?: number,
+    active?: boolean,
+    createdAt?: Date,
+    updatedAt?: Date,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.ingredients = ingredients;
+    this.caffeinated = caffeinated;
+    this.caffeine = caffeine;
+    this.containsAlcohol = containsAlcohol;
+    this.calories = calories;
+    this.volume = volume;
+    this.active = active;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
 }
